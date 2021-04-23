47adce530e9fa0ecc107c66d6077d6d00d8fee55
yasminwaldeck <yasmin.waldeck@posteo.de>
2021-04-23 10:41:22 +0200

    Set up methods and first test

--- src/test/java/TestPasswordValidator.java
+++ src/test/java/TestPasswordValidator.java
@@ -1,2 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class TestPasswordValidator {
+
+    @Test
+    public void validateLengthTrue(){
+        //GIVEN
+        String password1 = "12345789";
+        int minLength1 = 4;
+        String password2 = "12345789";
+        int minLength2 = 8;
+        //WHEN
+        boolean test1 = PasswordValidator.validateLength(password1, minLength1);
+        boolean test2 = PasswordValidator.validateLength(password2, minLength2);
+
+        //THEN
+
+        assertTrue(test1);
+        assertTrue(test2);
+
+    }
+
+    @Test
+    public void validateLengthFalse(){
+
+    }
+
 }
